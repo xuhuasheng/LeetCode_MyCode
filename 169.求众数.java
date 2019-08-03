96bfbdb8a1a587d2ad339de211e8b40d64cff456
xuhuasheng <xu_watson@163.com>
2019-08-03 10:15:26 +0800

    [169]求众数

--- 169.求众数.java
+++ 169.求众数.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /*
  * @lc app=leetcode.cn id=169 lang=java
  *
@@ -5,7 +7,21 @@
  */
 class Solution {
     public int majorityElement(int[] nums) {
-        
+        int majorityElement = 0;
+        int halfLength = nums.length/2;
+        HashMap<Integer, Integer> numsMap = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            if (!numsMap.containsKey(num)) {
+                numsMap.put(num, 1);
+            } else {
+                numsMap.put(num, numsMap.get(num)+1);
+            }
+            if (numsMap.get(num) > halfLength) {
+                majorityElement = num;
+                break;
+            }
+        }
+        return majorityElement;
     }
 }
 
